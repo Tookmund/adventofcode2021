f6d5aaa29211a48896e3e79936f24dfddcc13d38
Jacob Adams <jacob@tookmund.com>
2021-12-02 00:34:21 -0500

    day2: part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -6,15 +6,19 @@ type Depth = i32;
 fn main() -> io::Result<()>{
     let mut h = 0;
     let mut d = 0;
+    let mut aim = 0;
     for line in io::stdin().lock().lines() {
         let l = line?;
         let s = l.split_whitespace().collect::<Vec<_>>();
         let command = s[0];
         let num = s[1].parse::<Depth>().unwrap();
         match command {
-            "forward" => h += num,
-            "down" => d += num,
-            "up" => d -= num,
+            "forward" => {
+                h += num;
+                d += aim*num;
+            }
+            "down" => aim += num,
+            "up" => aim -= num,
             _ => panic!("Invalid command {}", command),
         }
     }
