6d972ff0217dce1f7245aa474298891279d1691a
Jacob Adams <jacob@tookmund.com>
2021-12-02 00:31:27 -0500

    day2: part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,23 @@
+use std::io;
+use std::io::prelude::*;
+
+type Depth = i32;
+
+fn main() -> io::Result<()>{
+    let mut h = 0;
+    let mut d = 0;
+    for line in io::stdin().lock().lines() {
+        let l = line?;
+        let s = l.split_whitespace().collect::<Vec<_>>();
+        let command = s[0];
+        let num = s[1].parse::<Depth>().unwrap();
+        match command {
+            "forward" => h += num,
+            "down" => d += num,
+            "up" => d -= num,
+            _ => panic!("Invalid command {}", command),
+        }
+    }
+    println!("Horizontal: {}\nDepth: {}\nMultiplied: {}", h, d, h*d);
+    Ok(())
+}
