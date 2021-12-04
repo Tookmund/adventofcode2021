2fd1db6a89e381942577e07a638d81a8b1f6d748
Jacob Adams <jacob@tookmund.com>
2021-12-04 15:44:31 -0500

    day4: part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -12,8 +12,14 @@ enum State {
 type Num = usize;
 
 #[derive(Debug)]
+struct Cell {
+    num: Num,
+    mark: bool,
+}
+
+#[derive(Debug)]
 struct Board {
-    board: Vec<Vec<Num>>,
+    board: Vec<Vec<Cell>>,
     lookup: HashMap<Num, (usize, usize)>,
 }
 
@@ -29,18 +35,54 @@ impl Board {
             line.split(' ')
             .filter(|n| !n.is_empty())
             .map(|n| n.parse::<usize>().unwrap())
+            .map(|n| Cell { num: n, mark: false })
             .collect::<Vec<_>>()
         );
         let row = self.board.len()-1;
         for (col, val) in self.board.last().unwrap().iter().enumerate() {
-            self.lookup.insert(*val, (row, col));
+            self.lookup.insert(val.num, (row, col));
         }
     }
-    fn mark(&self, n: Num) -> bool {
-        true
+    // Returns true if marking that number causes this board to win
+    fn mark(&mut self, n: Num) -> bool {
+        match self.lookup.get(&n) {
+            None => false,
+            Some((r, c)) if self.board[*r][*c].mark == true => panic!("Marking cell ({}, {}) twice!", r, c),
+            Some((r, c)) => {
+                self.board[*r][*c].mark = true;
+                self.check(*r, *c)
+            }
+        }
     }
-    fn check(&self, r: Num, c: Num) {
-        ()
+    fn check(&self, r: Num, c: Num) -> bool {
+        if self.board[r][c].mark != true {
+            panic!("Checking unmarked cell ({}, {})!", r, c);
+        }
+        self.check_row(r) || self.check_col(c)
+    }
+    fn check_col(&self, c: Num) -> bool {
+        let mut win = true;
+        for ir in 0..self.board.len() {
+            if !self.board[ir][c].mark {
+                win = false;
+            }
+        }
+        win
+    }
+    fn check_row(&self, r: Num) -> bool {
+        let mut win = true;
+        for ic in 0..self.board[0].len() {
+            if !self.board[r][ic].mark {
+                win = false;
+            }
+        }
+        win
+    }
+    fn sum_unmarked(&self) -> Num {
+        self.board.iter().flatten()
+            .filter(|n| n.mark == false)
+            .map(|n| n.num)
+            .sum()
     }
 }
 
@@ -61,23 +103,28 @@ fn get_draws(line: &str) -> Vec<Num> {
 
 fn main() -> io::Result<()>{
     let mut curstate = State::Draw;
-    let mut draws: Vec<Num>;
+    let mut draws: Vec<Num> = Vec::new();
     let mut boards = Vec::<Board>::new();
 
     for line in io::stdin().lock().lines() {
         let line = line?;
         curstate = change_state(curstate, &line);
         match curstate {
-            State::Draw => {
-                draws = get_draws(&line);
-                println!("{:?}", draws);
-            },
+            State::Draw => draws = get_draws(&line),
             State::NewBoard => boards.push(Board::new()),
             State::PopulateBoard => {
                 boards.last_mut().unwrap().populate(&line);
             }
         }
     }
-    println!("{:?}", boards);
+
+    for d in draws {
+        for b in boards.iter_mut() {
+            if b.mark(d) {
+                println!("Final Score: {}", b.sum_unmarked()*d);
+                return Ok(());
+            }
+        }
+    }
     Ok(())
 }
