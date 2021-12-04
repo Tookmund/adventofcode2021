d9e959d382b777acc45b425c4bf1a14c44128149
Jacob Adams <jacob@tookmund.com>
2021-12-03 22:36:42 -0500

    day3: part 2 whole list, incorrect

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,8 +4,11 @@ use std::io::prelude::*;
 fn main() -> io::Result<()>{
     let mut zero = Vec::<usize>::new();
     let mut one = Vec::<usize>::new();
-    for line in io::stdin().lock().lines() {
-        for (i, c) in line?.chars().enumerate() {
+    let lines = io::stdin().lock().lines()
+        .map(|l| l.unwrap())
+        .collect::<Vec<_>>();
+    for line in &lines {
+        for (i, c) in line.chars().enumerate() {
             if i >= zero.len() {
                 zero.push(0);
                 one.push(0);
@@ -19,6 +22,7 @@ fn main() -> io::Result<()>{
     }
     let mut gamma = String::new();
     let mut epsilon = String::new();
+
     for i in 0..zero.len() {
         if zero[i] > one[i] {
             gamma += "0";
@@ -28,9 +32,45 @@ fn main() -> io::Result<()>{
             epsilon += "0";
         }
     }
-    let gamma_num = usize::from_str_radix(&gamma, 2).unwrap();
-    let epsilon_num = usize::from_str_radix(&epsilon, 2).unwrap();
+    let gamma_num = bin(&gamma);
+    let epsilon_num = bin(&epsilon);
     println!("Gamma: {}\nEpsilon: {}\nMultiplied: {}",
              gamma_num, epsilon_num, gamma_num*epsilon_num);
+
+    let mut oxygen = lines.clone();
+    let mut co2 = lines.clone();
+
+    println!("Oxygen");
+    let oxygen_num = rating(&zero, &one, &mut oxygen, '0', '1');
+    println!("CO2");
+    let co2_num = rating(&zero, &one, &mut co2, '1', '0');
+    println!("Oxygen: {}\nCO2: {}\nMultiplied: {}",
+             oxygen_num, co2_num, oxygen_num*co2_num);
     Ok(())
+}
+
+fn bin(n: &str) -> usize {
+    usize::from_str_radix(n, 2).unwrap()
+}
+
+fn rating(zero: &[usize], one: &[usize], vec: &mut Vec<String>,
+             z: char, o: char) -> usize {
+    for i in 0..zero.len() {
+        if zero[i] > one[i] {
+            println!("0 at {}", i);
+            vec.retain(|n| get_char(n, i) == z);
+        } else {
+            println!("1 at {}", i);
+            vec.retain(|n| get_char(n, i) == o);
+        }
+        println!("{:?}", vec);
+        if vec.len() == 1 {
+            return bin(&vec[0]);
+        }
+    }
+    panic!("Could not find rating!")
+}
+
+fn get_char(s: &String, i: usize) -> char {
+    s.chars().nth(i).unwrap()
 }
