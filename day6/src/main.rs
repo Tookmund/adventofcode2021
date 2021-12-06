adcaf7a094d53a92edd79c0fb945e7e346194079
Jacob Adams <jacob@tookmund.com>
2021-12-06 00:48:48 -0500

    day6: part 2 the smart way

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 #[derive(Debug)]
 struct Lantern {
     days: usize,
+    numfish: usize,
 }
 
 const RESET: usize = 6;
@@ -11,18 +12,19 @@ const NEW: usize = 8;
 const DAYS: usize = 256;
 
 impl Lantern {
-    fn new(days: usize) -> Self {
+    fn new(days: usize, numfish: usize) -> Self {
         Lantern {
-            days: days
+            days: days,
+            numfish: numfish,
         }
     }
-    fn day(&mut self) -> Option<Lantern> {
+    fn day(&mut self) -> usize {
         if self.days == 0 {
             self.days = RESET;
-            Some(Self::new(NEW))
+            self.numfish
         } else {
             self.days -= 1;
-            None
+            0
         }
     }
 }
@@ -31,21 +33,19 @@ fn main() -> io::Result<()> {
     for line in io::stdin().lock().lines() {
         let mut fish = line?.split(',')
             .map(|n| n.parse::<usize>().unwrap())
-            .map(|n| Lantern::new(n))
+            .map(|n| Lantern::new(n, 1))
             .collect::<Vec<Lantern>>();
         println!("Initial state: {:?}", fish);
         for i in 1..=DAYS {
-            let mut newfish: Vec<Lantern> = Vec::new();
+            let mut newfish: usize = 0;
             for f in fish.iter_mut() {
-                match f.day() {
-                    Some(v) => newfish.push(v),
-                    None => ()
-                }
+                newfish += f.day();
             }
-            fish.extend(newfish);
-            println!("Day {}", i);
+            if newfish > 0 {
+                fish.push(Lantern::new(NEW, newfish));
+            }
         }
-        println!("Total after {} days: {}", DAYS, fish.len());
+        println!("Total after {} days: {}", DAYS, fish.iter().map(|f| f.numfish).sum::<usize>());
     }
     Ok(())
 }
