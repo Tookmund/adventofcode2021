83bb38683db15faea35d1bbfb00913a0aa0a15ba
Jacob Adams <jacob@tookmund.com>
2021-12-10 20:13:19 -0500

    stdin.rs: Cleverer stdin template

--- stdin.rs
+++ stdin.rs
@@ -1,9 +1,29 @@
 use std::io;
 use std::io::prelude::*;
 
-fn main() -> io::Result<()>{
-    for line in io::stdin().lock().lines() {
+fn function<B: io::BufRead>(bufread: B) -> io::Result<usize> {
+    Ok(0)
+}
 
+#[cfg(test)]
+mod test {
+    use crate::function;
+
+    const EXAMPLE: &[u8] = b"\
+TEST\n\
+Data\n\
+Here\n";
+
+    #[test]
+    fn test_example() {
+        assert_eq!(function(EXAMPLE).unwrap(), 0)
     }
+
+}
+
+fn main() -> io::Result<()>{
+    //let mut stdin = Vec::new();
+    //io::stdin().read_to_end(&mut stdin)?;
+    println!("Function: {}", function(io::stdin().lock())?);
     Ok(())
 }
