8247329b6c3c798a80b0aa5e87921cd83d6abc11
Jacob Adams <jacob@tookmund.com>
2022-03-06 12:26:39 -0500

    stdin.rs: Make test example with macros

--- stdin.rs
+++ stdin.rs
@@ -9,16 +9,32 @@ fn function<B: io::BufRead>(bufread: B) -> io::Result<usize> {
 mod test {
     use crate::function;
 
-    const EXAMPLE: &[u8] = b"\
-TEST\n\
-Data\n\
-Here\n";
-
-    #[test]
-    fn test_example() {
-        assert_eq!(function(EXAMPLE).unwrap(), 0)
+    macro_rules! make_tests {
+        ( $func:expr, $( $name:ident, $input:expr, $output:expr ), +) => {
+            $(
+                #[test]
+                fn $name () {
+                    assert_eq!($func($input.as_bytes()).unwrap(), $output);
+                }
+             )*
+        }
     }
 
+    macro_rules! make_tests_method {
+        ( $func:expr, $( $name:ident, $input:expr, $method:ident, $output:expr ), +) => {
+            $(
+                #[test]
+                fn $name () {
+                    assert_eq!($func($input.as_bytes()).unwrap().$method(), $output);
+                }
+             )*
+        }
+    }
+
+    make_tests! {
+        function,
+            example_name, "EXAMPLE DATA", 0 //EXAMPLE RESULT
+    }
 }
 
 fn main() -> io::Result<()>{
